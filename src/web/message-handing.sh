8d034467f1b13e71959f7905867770babd89cd66
Dirk Hohndel <dirk@hohndel.org>
2024-01-23 23:56:15 +0000

    translations: add simple wrapper script
    This really is intended to make sure I don't forget how to do the different
    steps of dealing with the translations.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ src/web/message-handing.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# super cheesy way to make sure that I don't forget how to deal with the translations
+cd $(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")
+
+while [[ $# > 0 ]] ; do
+    case "$1" in
+    init)
+        if [[ "$2" != "" ]]; then
+            if [[ -d translations/"$2" ]] ; then
+                echo "we already have a translations/$2 directory... if you really want to init, do it manually"
+                exit 1
+            fi
+            pybabel init -i messages.pot -d translations -l "$2"
+            shift
+        else
+            echo "init needs the language designation as argument"
+            exit 1
+        fi
+        ;;
+    update)
+        if [[ "$2" != "" ]]; then
+            pybabel update -i messages.pot -d translations -l "$2"
+            shift
+        else
+            (
+                cd translations
+                for t in $(find . -maxdepth 1 -type d -name ??\*); do
+                    pybabel update -i messages.pot -d translations -l "$t"
+                done
+            )
+        fi
+        ;;
+    compile)
+        pybabel compile -d translations
+        ;;
+    extract)
+        pybabel extract -F babel.config -o messages.pot .
+    esac
+    shift
+done
