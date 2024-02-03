8f552087d6f806d3bbc5aef6d4d5a43f78bd6efe
Dirk Hohndel <dirk@hohndel.org>
2024-02-03 23:44:32 +0000

    add helper script to make current releases
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ src/web/make-current.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set things up for a current release
+#
+# this doesn't include (of course) the signing of the Windows and macOS installers
+# also - you'll need to restart the docker container for the website afterwards
+
+croak() {
+    echo "$*" ; exit 1
+}
+
+cd $(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")") || croak "can't cd to script dir"
+
+STATICPATH=/data/www/subsurfacestaticsite/downloads
+
+[[ $1 == "" ]] && croak "Usage: ${BASH_SOURCE[0]} buildnr"
+
+BUILDNR="$1"
+TODAY=$(date +%Y-%m-%d)
+
+sed -i '/^crelease/d' persistent.store
+echo "crelease=$BUILDNR" >> persistent.store
+echo "crelease_date=$TODAY" >> persistent.store
+
+cd $STATICPATH || croak "can't cd to $STATICPATH"
+for f in subsurface-6.0.${BUILDNR}-CICD-release-installer.exe \
+        Subsurface-6.0.${BUILDNR}-CICD-release.dmg \
+        Subsurface-mobile-6.0.${BUILDNR}-CICD-release.apk \
+        Subsurface-v6.0.${BUILDNR}-CICD-release.AppImage
+do
+    wget "https://github.com/subsurface/nightly-builds/releases/download/v6.0.${BUILDNR}-CICD-release/$f"
+done
